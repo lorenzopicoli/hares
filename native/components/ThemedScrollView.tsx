d8746071d4dc2a73c166006240da18f804341cd2
Lorenzo <lorenzo@Lorenzos-MacBook-Air.local>
2025-01-19 19:20:05 -0500

    fix build

--- native/components/ThemedScrollView.tsx
+++ native/components/ThemedScrollView.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 import Animated, { useAnimatedRef } from "react-native-reanimated";
 
 import { ThemedView } from "@/components/ThemedView";
-import { useBottomTabOverflow } from "@/components/ui/TabBarBackground";
+import { useBottomTabOverflow } from "@/hooks/useBottomTabOverflow";
 
 type Props = PropsWithChildren;
 
